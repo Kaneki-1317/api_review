71ddb13bc8f87511b2d023056edbaf1ba739bb83
Cristian Mayorga <cristiandres1317@gmail.com>
2026-04-27 09:16:21 -0500

    API simple de practica

---
+++ api.py
@@ -0,0 +1,28 @@
+from flask import Flask, Response
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def get_user():
+    users = [
+        {"nombre": "Cristian", "apellido": "Mayorga", "telefono": 3150606018},
+        {"nombre": "Diego", "apellido": "Diaz", "telefono": 3150606018},
+        {"nombre": "Dayana", "apellido": "Barbosa", "telefono": 3150606018}
+    ]
+
+    data = {
+        "status": "success",
+        "total": len(users),
+        "data": {
+            "users": users
+        }
+    }
+
+    return Response(
+        json.dumps(data, indent=2),
+        mimetype='application/json'
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
